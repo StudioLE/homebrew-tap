1b4e1f8e27b47a6403f87a105660a6bb51a4a7f4
StudioLE <StudioLE@users.noreply.github.com>
2026-01-10 01:05:41 +0000

    feat: Update `alnwick` to `v0.3.0`

--- Formula/alnwick.rb
+++ Formula/alnwick.rb
@@ -7,20 +7,20 @@ class Alnwick < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/StudioLE/alnwick/releases/download/v0.3.0/alnwick-0.3.0-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "557d1c3cebbc587d569bd2029291531bf07660b9eaab0923dc9373c63913437a"
+      sha256 "97ca422aad4125c6c3d3c790abb54a238d6528d19d403f0097d9968a63b81204"
     else
       url "https://github.com/StudioLE/alnwick/releases/download/v0.3.0/alnwick-0.3.0-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "14296451f715c3e54c287bb85009f3bdf0137dbaa263ec00e08ba58eacbaeb8b"
+      sha256 "b2ad8b3c31b323aef30cd0b16b5acf5b48a610211b6cb90da27c15d05dd24355"
     end
   end
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/StudioLE/alnwick/releases/download/v0.3.0/alnwick-0.3.0-aarch64-apple-darwin.tar.xz"
-      sha256 "74585bbe9cbdd67f0a5e705838c099ffaa8b56e7b187aa9110ca776e93ee6f32"
+      sha256 "ce81cffbb933f62d685bc197940ca2e4e0c81acca4eb6a6230bf68f9027ebb40"
     else
       url "https://github.com/StudioLE/alnwick/releases/download/v0.3.0/alnwick-0.3.0-x86_64-apple-darwin.tar.xz"
-      sha256 "aca408f81f3c22d8007d1b6d79506c2c7f3a6d6ad5c620c3c18066b10882ae5f"
+      sha256 "d45d5ba3da0816268ea3e8c62265805762c1f1637255695970099e1ee57f38e2"
     end
   end
 
