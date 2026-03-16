b50419758ad89c040e708a0f57063074d2671359
StudioLE <StudioLE@users.noreply.github.com>
2026-03-16 22:01:33 +0000

    feat: Update `hook-rs` to `v0.4.0`

--- Formula/hook-rs.rb
+++ Formula/hook-rs.rb
@@ -7,20 +7,20 @@ class HookRs < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/StudioLE/hook-rs/releases/download/v0.4.0/hook-rs-0.4.0-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "ff3498a9637fe69d1a3e364023ebc3a6eba6f30aee6cfd0c00e9e3364a9fc2d4"
+      sha256 "a03799aba07110ab27cfeb97aa00e11109a76b1b7e9b4a61b1c1985e362808a3"
     else
       url "https://github.com/StudioLE/hook-rs/releases/download/v0.4.0/hook-rs-0.4.0-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "a86db5de1ae85fd433ae90ab4255bdd70ad6859a5ef32226be97703361550ac4"
+      sha256 "7336736838cd4776b1fb380e4d841ddf6ad329e2da99e0f2c83bb696acef97c8"
     end
   end
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/StudioLE/hook-rs/releases/download/v0.4.0/hook-rs-0.4.0-aarch64-apple-darwin.tar.xz"
-      sha256 "d865d906ba06ffb753af10de77f3ac0d88fdbd47a3fe8efd524024f2631cedda"
+      sha256 "162767568ab4c44620c95eb0f18617bdd4d033083c202bb7c7758d0cc3d8bcfe"
     else
       url "https://github.com/StudioLE/hook-rs/releases/download/v0.4.0/hook-rs-0.4.0-x86_64-apple-darwin.tar.xz"
-      sha256 "c80ee1efb50e69b226550e21f016e449baf7f7892d63f071e6870ea4a4b1675a"
+      sha256 "5a8fa18f0352df8084676b0da1bae2d4180eb7a882b51dc00d0a54c0df8fa3d9"
     end
   end
 
