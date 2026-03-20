dcccb696243b6a247bcb0a6049923a858391dd24
StudioLE <StudioLE@users.noreply.github.com>
2026-03-20 15:26:26 +0000

    feat: Update `hook-rs` to `v0.6.1`

--- Formula/hook-rs.rb
+++ Formula/hook-rs.rb
@@ -2,25 +2,25 @@ class HookRs < Formula
   desc "Permission hooks for Claude Code with bash-aware command analysis"
   homepage "https://github.com/StudioLE/hook-rs"
   license "AGPL-3.0-only"
-  version "0.6.0"
+  version "0.6.1"
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/StudioLE/hook-rs/releases/download/v0.6.0/hook-rs-0.6.0-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "4a86ce7ac2d30f9cd03cc79432b12d4bee2128ed6ecd32ad1ddd585d33c1ac1d"
+      url "https://github.com/StudioLE/hook-rs/releases/download/v0.6.1/hook-rs-0.6.1-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "09504c0189ebde0f9cd3745649c3e25edad6c44c2b8f503fe129eb11823c08b1"
     else
-      url "https://github.com/StudioLE/hook-rs/releases/download/v0.6.0/hook-rs-0.6.0-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "fc82cb06c370d2c45e6be618059ef16a471c168924694a627713edf22902224d"
+      url "https://github.com/StudioLE/hook-rs/releases/download/v0.6.1/hook-rs-0.6.1-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "a787ab3f7e4bb3082e9dc8ee8c93d1f3b720cb3f2be0c66da0ecd189a710c8d2"
     end
   end
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/StudioLE/hook-rs/releases/download/v0.6.0/hook-rs-0.6.0-aarch64-apple-darwin.tar.xz"
-      sha256 "272183fed00f7d5203599ce70550401f6b49bf4a60e672bee20923a2245ea383"
+      url "https://github.com/StudioLE/hook-rs/releases/download/v0.6.1/hook-rs-0.6.1-aarch64-apple-darwin.tar.xz"
+      sha256 "dd2a0a44af7b69679239fb5459aa4b14a3e097836a6baccc1bd095390d65e059"
     else
-      url "https://github.com/StudioLE/hook-rs/releases/download/v0.6.0/hook-rs-0.6.0-x86_64-apple-darwin.tar.xz"
-      sha256 "851a82a4480f5ab8cc9702b55401e741de42975d715df626e4f50cead0efb12f"
+      url "https://github.com/StudioLE/hook-rs/releases/download/v0.6.1/hook-rs-0.6.1-x86_64-apple-darwin.tar.xz"
+      sha256 "867f88705964f31d6845722e0d1609e30dac329fd5b382590318eda2a912e919"
     end
   end
 
