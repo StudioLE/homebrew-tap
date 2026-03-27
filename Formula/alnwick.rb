0fc175662482550176e3da74bb0cc93ac661af16
StudioLE <StudioLE@users.noreply.github.com>
2026-03-27 22:35:17 +0000

    feat: Update `alnwick` to `v0.4.0`

--- Formula/alnwick.rb
+++ Formula/alnwick.rb
@@ -2,25 +2,25 @@ class Alnwick < Formula
   desc "A self hosted podcast library"
   homepage "https://github.com/StudioLE/alnwick"
   license "GPL-3.0-only"
-  version "0.3.0"
+  version "0.4.0"
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/StudioLE/alnwick/releases/download/v0.3.0/alnwick-0.3.0-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "97ca422aad4125c6c3d3c790abb54a238d6528d19d403f0097d9968a63b81204"
+      url "https://github.com/StudioLE/alnwick/releases/download/v0.4.0/alnwick-0.4.0-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "0dfdc1278a2c076a891b2429f597e58a2487249a0df1be3362bec2cbade51045"
     else
-      url "https://github.com/StudioLE/alnwick/releases/download/v0.3.0/alnwick-0.3.0-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "b2ad8b3c31b323aef30cd0b16b5acf5b48a610211b6cb90da27c15d05dd24355"
+      url "https://github.com/StudioLE/alnwick/releases/download/v0.4.0/alnwick-0.4.0-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "01902816d3f540c8ecb7de861c14a9a1d793e0705bcaf51781644c746ed72641"
     end
   end
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/StudioLE/alnwick/releases/download/v0.3.0/alnwick-0.3.0-aarch64-apple-darwin.tar.xz"
-      sha256 "ce81cffbb933f62d685bc197940ca2e4e0c81acca4eb6a6230bf68f9027ebb40"
+      url "https://github.com/StudioLE/alnwick/releases/download/v0.4.0/alnwick-0.4.0-aarch64-apple-darwin.tar.xz"
+      sha256 "09ee7ef253d0a3253c4c89c61e59ce3735fa3224e28e9cc99ae71cffe99593ce"
     else
-      url "https://github.com/StudioLE/alnwick/releases/download/v0.3.0/alnwick-0.3.0-x86_64-apple-darwin.tar.xz"
-      sha256 "d45d5ba3da0816268ea3e8c62265805762c1f1637255695970099e1ee57f38e2"
+      url "https://github.com/StudioLE/alnwick/releases/download/v0.4.0/alnwick-0.4.0-x86_64-apple-darwin.tar.xz"
+      sha256 "ec99e24c1d4d070eaa7afb995ea49d3f5dd28c576d98c8817109c47f839aae9a"
     end
   end
 
