b9df7c0624cc2a4526cd6fad2486ed7970a16ff3
StudioLE <StudioLE@users.noreply.github.com>
2026-03-28 11:18:01 +0000

    feat: Update `mount-luks` to `v0.6.2`

--- Formula/mount-luks.rb
+++ Formula/mount-luks.rb
@@ -2,15 +2,15 @@ class MountLuks < Formula
   desc "A simple CLI tool to unlock and mount a LUKS encrypted disk."
   homepage "https://github.com/StudioLE/mount-luks"
   license "GPL-3.0-only"
-  version "0.6.1"
+  version "0.6.2"
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/StudioLE/mount-luks/releases/download/v0.6.1/mount-luks-0.6.1-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "ad3f2d4071f61567f5dba400ce62c6218443b6518ce14b2ef04b88791c92cb8e"
+      url "https://github.com/StudioLE/mount-luks/releases/download/v0.6.2/mount-luks-0.6.2-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "46c34dae432be04ac0048bd96618f93444df1de5b223edd9bf4dedf745da0da6"
     else
-      url "https://github.com/StudioLE/mount-luks/releases/download/v0.6.1/mount-luks-0.6.1-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "29ff577c37b7dd185ee5b8b480438c72d2e9c8f132401dc63df0019fee6525e2"
+      url "https://github.com/StudioLE/mount-luks/releases/download/v0.6.2/mount-luks-0.6.2-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "f6edb640e8f822e9419b14fa5e8a645dd4850cb5cf89d38e7b4f936ca7d4991f"
     end
   end
 
