7d3becafda9a28299fca570aef42806c255de3d3
StudioLE <StudioLE@users.noreply.github.com>
2026-04-25 13:23:27 +0000

    feat: Update `hook-rs` to `v0.10.0`

--- Formula/hook-rs.rb
+++ Formula/hook-rs.rb
@@ -2,25 +2,25 @@ class HookRs < Formula
   desc "Permission hooks for Claude Code with bash-aware command analysis"
   homepage "https://github.com/StudioLE/hook-rs"
   license "AGPL-3.0-only"
-  version "0.9.0"
+  version "0.10.0"
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/StudioLE/hook-rs/releases/download/v0.9.0/hook-rs-0.9.0-aarch64-unknown-linux-gnu.tar.xz"
-      sha256 "4187046f0541e8ccac122ed2d0df608a46033ba4648e13932d73e863cb6a85db"
+      url "https://github.com/StudioLE/hook-rs/releases/download/v0.10.0/hook-rs-0.10.0-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "fb78628cfb05697232cce4a309c93db667fa03c2ce14b2f8d5d37e128b9df185"
     else
-      url "https://github.com/StudioLE/hook-rs/releases/download/v0.9.0/hook-rs-0.9.0-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "005d5aab77a4820221fbfe921fc719180cd074d41508fd8c60f8fb6f4faf6e86"
+      url "https://github.com/StudioLE/hook-rs/releases/download/v0.10.0/hook-rs-0.10.0-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "e3988546a86118cb6b655af5de3a7df07f9b3b6c76db0db0925b11c98c0425a5"
     end
   end
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/StudioLE/hook-rs/releases/download/v0.9.0/hook-rs-0.9.0-aarch64-apple-darwin.tar.xz"
-      sha256 "f640f40988456a39d3beac3a3f89cfda4e3f5a79be9c9ae009c5fb05a86bc766"
+      url "https://github.com/StudioLE/hook-rs/releases/download/v0.10.0/hook-rs-0.10.0-aarch64-apple-darwin.tar.xz"
+      sha256 "ce6526b1652e4750fe0cb6220d7602d74423c708b91ebb73160f3e55ac114e71"
     else
-      url "https://github.com/StudioLE/hook-rs/releases/download/v0.9.0/hook-rs-0.9.0-x86_64-apple-darwin.tar.xz"
-      sha256 "7cacb6ef8fbeb59dca67aa47056ed0aea3a21ebfaf0d9d776ccd3659792c853a"
+      url "https://github.com/StudioLE/hook-rs/releases/download/v0.10.0/hook-rs-0.10.0-x86_64-apple-darwin.tar.xz"
+      sha256 "8bddd4dd1393b9a297edaf4152fff78f31417583a3d5d7f117a0dfb0064cab27"
     end
   end
 
